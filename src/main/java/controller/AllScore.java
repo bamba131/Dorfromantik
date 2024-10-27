be742537cbe501afedb38ae2e22c5bf309b371b1
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:00:31 +0100

    Actualiser src/main/java/controller/AllScore.java

--- src/main/java/controller/AllScore.java
+++ src/main/java/controller/AllScore.java
@@ -7,17 +7,33 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ * La classe AllScore est responsable de la récupération des scores pour une série spécifique depuis la base de données.
+ * Elle utilise JDBC pour se connecter à une base de données MariaDB, exécuter une requête SQL, et retourner les scores 
+ * sous forme de liste d'entiers.
+ */
 public class AllScore {
+
+     /**
+     * Récupère les scores pour une série donnée, identifiée par son identifiant, depuis la base de données.
+     * Les scores sont récupérés en ordre décroissant et limités aux 10 meilleurs scores.
+     *
+     * @param idSerie l'identifiant de la série pour laquelle récupérer les scores
+     * @return une liste d'entiers contenant les scores de la série, classés par ordre décroissant
+     * @throws RuntimeException si le pilote JDBC n'est pas trouvé
+     * @throws SQLException si une erreur survient lors de la connexion ou de l'exécution de la requête SQL
+     */
     public static ArrayList<Integer> getScoresForSeries(int idSerie) {
         ArrayList<Integer> scores = new ArrayList<>();
 
         try {
+            // Chargement du driver JDBC pour MariaDB
             Class.forName("org.mariadb.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.err.println("Erreur : pilote JDBC non trouvé");
             System.exit(1);
         }
-
+        // Préparation de la requête SQL pour récupérer les scores
         try (Connection cnx = DriverManager.getConnection(
                 "jdbc:mariadb://dwarves.iut-fbleau.fr/akagundu",
                 "akagundu", "dersim62Lodek")) {
