6aacd0d59a1ee9c1e541d25115d25a485e3833ae
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:30:37 +0100

    Actualiser src/main/java/controller/QuiListener.java

--- src/main/java/controller/QuiListener.java
+++ src/main/java/controller/QuiListener.java
@@ -3,7 +3,18 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * La classe QuiListener implémente ActionListener et gère l'action de fermeture de l'application.
+ * Lorsque l'événement est déclenché, l'application se termine.
+ */
 public class QuiListener implements ActionListener {
+
+    /**
+     * Appelé lorsque l'action est déclenchée.
+     * Cette méthode termine l'application en appelant System.exit(0).
+     *
+     * @param e l'événement d'action qui a été déclenché
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         System.exit(0);
