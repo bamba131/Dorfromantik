7f0d0f3ae0429f1263ec1046c782fe1effebfe23
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:23:34 +0100

    Rajout Javadoc

--- src/main/java/controller/MouseDragHandler.java
+++ src/main/java/controller/MouseDragHandler.java
@@ -5,14 +5,32 @@ import java.awt.event.MouseEvent;
 import java.awt.Point;
 import javax.swing.SwingUtilities;
 
+
+/**
+ * La classe MouseDragHandler gère les événements de glisser-déposer pour déplacer la vue d'une grille.
+ * Elle utilise un {@link CameraControllerListener} pour ajuster l'offset de la vue en fonction des mouvements de souris.
+ */
 public class MouseDragHandler extends MouseAdapter {
 
     private CameraControllerListener listener;
 
+
+    /**
+     * Construit un gestionnaire de glissement de souris.
+     *
+     * @param listener le listener pour gérer les mises à jour de la vue de la grille en réponse au déplacement de la souris
+     */
     public MouseDragHandler(CameraControllerListener listener) {
         this.listener = listener;
     }
 
+
+    /**
+     * Appelé lorsque la souris est glissée avec le bouton droit enfoncé. 
+     * Calcule le déplacement et met à jour la vue via le listener.
+     *
+     * @param e l'événement de glissement de la souris
+     */
     @Override
     public void mouseDragged(MouseEvent e) {
         if (listener.getMouseDragStart() != null && SwingUtilities.isRightMouseButton(e)) {
@@ -28,6 +46,12 @@ public class MouseDragHandler extends MouseAdapter {
         }
     }
 
+    /**
+     * Appelé lorsque le bouton droit de la souris est relâché. 
+     * Réinitialise le point de départ du glissement.
+     *
+     * @param e l'événement de relâchement de la souris
+     */
     @Override
     public void mouseReleased(MouseEvent e) {
         if (SwingUtilities.isRightMouseButton(e)) {
