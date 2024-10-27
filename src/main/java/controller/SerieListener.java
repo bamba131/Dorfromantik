9aed71bb947f2eb12579e8862e4db34faced1713
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:44:06 +0100

    Rajout Javadoc

--- src/main/java/controller/SerieListener.java
+++ src/main/java/controller/SerieListener.java
@@ -3,15 +3,31 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * La classe SerieListener est un écouteur d'événements qui réagit aux actions de l'utilisateur
+ * concernant les séries de jeux. Elle charge une série spécifique lorsque l'utilisateur interagit.
+ */
 public class SerieListener implements ActionListener {
     private final GameController gameController;
     private final int seriesNumber;
 
+    /**
+     * Constructeur de SerieListener.
+     *
+     * @param gameController le contrôleur de jeu qui gère la logique de la série
+     * @param seriesNumber   le numéro de la série à charger
+     */
     public SerieListener(GameController gameController, int seriesNumber) {
         this.gameController = gameController;
         this.seriesNumber = seriesNumber;
     }
 
+     /**
+     * Méthode appelée lorsqu'un événement d'action se produit. Elle charge la série spécifiée
+     * dans le contrôleur de jeu.
+     *
+     * @param e l'événement d'action contenant les détails de l'événement
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         gameController.loadSeries(seriesNumber);
