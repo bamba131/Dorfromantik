b37df3229ed9ef398e9d57d73ae04967e577a867
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:45:15 +0100

    Rajout Javadoc

--- src/main/java/controller/SeriesSelector.java
+++ src/main/java/controller/SeriesSelector.java
@@ -3,8 +3,18 @@ package controller;
 import view.GameView;
 import javax.swing.SwingUtilities;
 
+/**
+ * La classe SeriesSelector est responsable de la sélection d'une série de jeux
+ * et du démarrage de la vue de jeu correspondante. Elle gère l'initialisation
+ * de l'interface graphique pour une série donnée.
+ */
 public class SeriesSelector {
 
+    /**
+     * Démarre le jeu avec la série spécifiée.
+     *
+     * @param seriesId l'identifiant de la série à charger et à jouer
+     */
     public void startGameWithSeries(int seriesId) {
         SwingUtilities.invokeLater(() -> new GameView(seriesId)); // Démarre GameView avec l'ID de série donné
         System.out.println("Série " + seriesId + " sélectionnée et jeu démarré.");
