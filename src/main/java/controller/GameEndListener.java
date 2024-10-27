bf714d6f0d8c1fc76e18586228baaf93df682fd1
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:15:27 +0100

    Rajout Javadoc

--- src/main/java/controller/GameEndListener.java
+++ src/main/java/controller/GameEndListener.java
@@ -1,5 +1,15 @@
 package controller;
 
+/**
+ * L'interface GameEndListener définit un écouteur pour la fin de la partie.
+ * Elle permet de notifier la fin de la partie et de transmettre le score final.
+ */
 public interface GameEndListener {
+
+    /**
+     * Méthode appelée lorsque la partie se termine.
+     *
+     * @param finalScore le score final atteint à la fin de la partie
+     */
     void onGameEnd(int finalScore);
 }
