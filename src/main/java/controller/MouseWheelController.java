47125d5f1854ed70d9d09aa5de94a46d7fb872f7
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:28:54 +0100

    Rajout Javadoc

--- src/main/java/controller/MouseWheelController.java
+++ src/main/java/controller/MouseWheelController.java
@@ -6,6 +6,10 @@ import view.HexagonTile;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
+/**
+ * La classe MouseWheelController gère les événements de rotation de la molette 
+ * de la souris pour faire pivoter la tuile en cours dans l'interface utilisateur.
+ */
 public class MouseWheelController implements MouseWheelListener {
 
     private HexagonTile previewTile;
@@ -13,11 +17,24 @@ public class MouseWheelController implements MouseWheelListener {
     private long lastRotationTime = 0;  // Stocke le temps de la dernière rotation
     private static final int ROTATION_DELAY = 100;  // Délai minimum en millisecondes entre chaque rotation
 
+    /**
+     * Construit un contrôleur de molette de souris.
+     *
+     * @param previewTile la tuile d'aperçu dont la rotation doit être contrôlée
+     * @param gameController le contrôleur de jeu qui gère l'état du jeu
+     */
     public MouseWheelController(HexagonTile previewTile, GameController gameController) {
         this.previewTile = previewTile;
         this.gameController = gameController;
     }
 
+     /**
+     * Appelé lorsque la molette de la souris est déplacée.
+     * Effectue la rotation de la tuile en cours en fonction de la direction 
+     * de la rotation de la molette.
+     *
+     * @param e l'événement de rotation de la molette de souris
+     */
     @Override
     public void mouseWheelMoved(MouseWheelEvent e) {
         long currentTime = System.currentTimeMillis();
