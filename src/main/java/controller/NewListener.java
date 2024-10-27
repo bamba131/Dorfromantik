0b24412ad69aa07e11255b37f079e7d949327d84
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:29:47 +0100

    Rajout Javadoc

--- src/main/java/controller/NewListener.java
+++ src/main/java/controller/NewListener.java
@@ -2,7 +2,18 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * La classe NewListener implémente ActionListener et gère les actions
+ * liées à la création d'un nouveau jeu. 
+ */
 public class NewListener implements ActionListener {
+
+    /**
+     * Appelé lorsque l'action est déclenchée. 
+     * Actuellement, cela imprime un message de test dans la console.
+     *
+     * @param e l'événement d'action qui a été déclenché
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("Juste pour tester - New Game");
