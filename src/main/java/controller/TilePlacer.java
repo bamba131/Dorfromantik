74cb4b1d9077f84c8b7f70ba6b03c237c358f268
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:45:55 +0100

    Rajout Javadoc

--- src/main/java/controller/TilePlacer.java
+++ src/main/java/controller/TilePlacer.java
@@ -2,6 +2,20 @@ package controller;
 
 import java.awt.Point;
 
+/**
+ * Interface TilePlacer.
+ * 
+ * Cette interface définit le comportement pour le placement de tuiles
+ * sur un plateau de jeu. Elle doit être implémentée par toute classe
+ * qui gère le placement des tuiles.
+ */
 public interface TilePlacer {
+
+     /**
+     * Place une tuile à la position spécifiée.
+     *
+     * @param position la position sur le plateau où la tuile doit être placée,
+     *                 représentée par un objet Point (coordonnées x et y)
+     */
     void placeTile(Point position);
 }
