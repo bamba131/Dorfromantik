63a89d940fa3da0bca97e8d6ab5923e1f5cec79e
Bamba TOP <bamba.top@etu.u-pec.fr>
2024-10-27 20:20:07 +0100

    Rajout Javadoc

--- src/main/java/controller/HowToPlayListener.java
+++ src/main/java/controller/HowToPlayListener.java
@@ -5,13 +5,28 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * La classe HowToPlayListener est un écouteur d'événements pour afficher la fenêtre 
+ * "Comment jouer" lors d'une action utilisateur, comme le clic sur un bouton d'aide.
+ * Elle affiche une boîte de dialogue {@link HowToPlayView} pour présenter les instructions de jeu.
+ */
 public class HowToPlayListener implements ActionListener {
     private JFrame parent;
 
+    /**
+     * Construit un écouteur HowToPlayListener.
+     *
+     * @param parent la fenêtre parent associée, utilisée comme référence pour afficher la boîte de dialogue
+     */
     public HowToPlayListener(JFrame parent) {
         this.parent = parent;
     }
 
+    /**
+     * Gère l'événement d'action déclenché, en affichant la fenêtre de dialogue "Comment jouer".
+     * 
+     * @param e l'événement d'action déclenché par l'utilisateur
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         HowToPlayView howToPlayDialog = new HowToPlayView(parent);
